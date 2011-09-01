a62f84593c6b41bc1a1e323b4f78c1d3e5c21022
YoFrankie <braun_markus@gmx.de>
2011-09-01 20:44:07 +0200

    blub

---
+++ src/de/slzm/MapSearch/MainMenu.java
@@ -0,0 +1,11 @@
+package de.slzm.MapSearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity{
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+		 setContentView(R.lay)
+	 }
+}
