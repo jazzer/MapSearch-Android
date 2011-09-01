18c3aad4fde0133cfd8c31a3fd1a4eec998ba075
YoFrankie <braun_markus@gmx.de>
2011-09-01 21:00:19 +0200

    buttons

--- src/de/slzm/MapSearch/MainMenu.java
+++ src/de/slzm/MapSearch/MainMenu.java
@@ -1,11 +1,30 @@
 package de.slzm.MapSearch;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainMenu extends Activity{
-	 @Override
-	 public void onCreate(Bundle savedInstanceState) {
-		 setContentView(R.lay)
-	 }
+public class MainMenu extends Activity {
+	
+	private Button buttonStart;
+	private Button buttonExit;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainmenu);
+		
+		buttonStart = (Button) findViewById(R.id.buttonstart);
+		buttonExit = (Button) findViewById(R.id.buttonexit);
+		
+		buttonStart.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(MainMenu.this, MapSearchActivity.class);
+				startActivity(i);
+			}
+		});
+	}
 }
