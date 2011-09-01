edd76565dbfd47f185ae53f2e0087558bb5bf44c
YoFrankie <braun_markus@gmx.de>
2011-09-01 23:08:06 +0200

    funzt

--- gen/de/slzm/MapSearch/R.java
+++ gen/de/slzm/MapSearch/R.java
@@ -13,12 +13,14 @@ public final class R {
     public static final class drawable {
         public static final int germany=0x7f020000;
         public static final int icon=0x7f020001;
+        public static final int marker=0x7f020002;
     }
     public static final class id {
-        public static final int buttonexit=0x7f050003;
-        public static final int buttonstart=0x7f050002;
-        public static final int map=0x7f050000;
-        public static final int mytextview=0x7f050001;
+        public static final int buttonexit=0x7f050004;
+        public static final int buttonstart=0x7f050003;
+        public static final int map=0x7f050001;
+        public static final int maplayout=0x7f050000;
+        public static final int marker=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/de/slzm/MapSearch/MapSearchActivity.java
+++ src/de/slzm/MapSearch/MapSearchActivity.java
@@ -2,31 +2,21 @@ package de.slzm.MapSearch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MapSearchActivity extends Activity {
 
-	private ImageView map;
-	private TextView mytext;
+	private MapLayout map;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		mytext = (TextView) findViewById(R.id.mytextview);
-		mytext.setText("blablub");
-
-		map = (ImageView) findViewById(R.id.map);
-		map.setOnTouchListener(new OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				mytext.setPadding((int) event.getX(), (int) event.getY(), 0, 0);
-				return false;
-			}
-		});
+		map = (MapLayout) findViewById(R.id.maplayout);
+		map.init();
 	}
+
+
 }
